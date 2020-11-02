18022b0eda64e73284af922e1646c307e0aa6d0a
tavisca-vnagaraju <vnagaraju@tavisca.in>
2020-11-02 11:43:37 +0530

    footer handling done

--- src/app/lit-elements/app.footer.litelement.ts
+++ src/app/lit-elements/app.footer.litelement.ts
@@ -18,12 +18,17 @@ class LitFooter extends LitElement {
             background-color: #673ab7;
             width: 100%;
             position: fixed;
-            bottom:-10px;
+            bottom:0px;
         }
         .footer-text{
             text-align:center;
         }
+        .margin-top-footer{
+          margin-bottom: 50px;
+          height: 20px;
+        }
       </style>
+      <div class="margin-top-footer"></div>
       <footer class="footer">
         <h4 class="footer-text"> ${this.footer}</h4>
       </footer>
